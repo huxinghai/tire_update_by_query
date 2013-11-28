24fc83f8ad63e77bcfb210f1b33140b1d963b57d
kaka <huxinghai1988@gmail.com>
2013-11-28 15:56:42 +0800

    fixed 算法

--- lib/tire_update_by_query/index.rb
+++ lib/tire_update_by_query/index.rb
@@ -10,8 +10,8 @@ module Tire
       _params = options.slice(:update)
       options.delete(:update)
       (options[:params] ||= {}).merge!(_params[:update])
-      format = Proc.new{|opts| opts.map{|k,v| v.is_a?(Hash) ? "#{k}.#{format.call(v).join('.')}" : k } }
-      options[:script] = format.call(_params[:update]).map{|v| "ctx._source.#{v}=#{v}" }.join(";")
+      format = lambda{|opts| opts.map{|k, v| v.is_a?(Hash) ? format.call(v).map{|vl| "#{k}.#{vl.is_a?(Array) ? vl.join('.') : vl}" } : k} }
+      options[:script] = format.call(_params[:update]).flatten.map{|v| "ctx._source.#{v}=#{v}" }.join(";")
       url = "#{self.url}/#{type}/_update_by_query"
       Configuration.client.post url, MultiJson.encode(options)
     ensure
