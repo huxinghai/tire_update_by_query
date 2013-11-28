ed5effe792b613ba01a932f6dd39e43a2839f28b
kaka <huxinghai1988@gmail.com>
2013-11-28 13:10:55 +0800

    fixed model class method

--- lib/tire_update_by_query.rb
+++ lib/tire_update_by_query.rb
@@ -1,25 +1,22 @@
 require "tire_update_by_query/version"
 require 'tire/index'
+require 'tire_update_by_query/index'
 
 module Tire
-  class Index
-    def update_by_query(type, options = {})
-      options.symbolize_keys!
-      raise ArgumentError, "Please pass a document type" unless type
-      raise ArgumentError, "Please pass a query" unless options.key?(:query)
-      unless options.key?(:script) || options.key?(:update)
-        raise ArgumentError, "Please pass a update or script"
+  module Model
+    module UpdateByQuery
+
+      def self.included(base)
+        base.extend(ClassMethods)
       end
-      _params = options.slice(:update)
-      options.delete(:update)
-      (options[:params] ||= {}).merge!(_params[:update])
-      format = Proc.new{|opts| opts.map{|k,v| v.is_a?(Hash) ? "#{k}.#{format.call(v).join('.')}" : k } }
-      options[:script] = format.call(_params[:update]).map{|v| "ctx._source.#{v}=#{v}" }.join(";")
-      url = "#{self.url}/#{type}/_update_by_query"
-      Configuration.client.post url, MultiJson.encode(options)
-    ensure
-      curl = %Q|curl -X POST "#{url}" -d '#{MultiJson.encode(options, :pretty => Configuration.pretty)}'|
-      logged('update_by_query', curl)
+
+      module ClassMethods
+        def index_update_by_query(*args)
+          options = args.extract_options!
+          _type = args[0] || self.tire.document_type
+          self.index.update_by_query(_type, options)
+        end
+      end
     end
   end
 end
---
+++ lib/tire_update_by_query/index.rb
@@ -0,0 +1,22 @@
+module Tire
+  class Index
+    def update_by_query(type, options = {})
+      options.symbolize_keys!
+      raise ArgumentError, "Please pass a document type" unless type
+      raise ArgumentError, "Please pass a query" unless options.key?(:query)
+      unless options.key?(:script) || options.key?(:update)
+        raise ArgumentError, "Please pass a update or script"
+      end
+      _params = options.slice(:update)
+      options.delete(:update)
+      (options[:params] ||= {}).merge!(_params[:update])
+      format = Proc.new{|opts| opts.map{|k,v| v.is_a?(Hash) ? "#{k}.#{format.call(v).join('.')}" : k } }
+      options[:script] = format.call(_params[:update]).map{|v| "ctx._source.#{v}=#{v}" }.join(";")
+      url = "#{self.url}/#{type}/_update_by_query"
+      Configuration.client.post url, MultiJson.encode(options)
+    ensure
+      curl = %Q|curl -X POST "#{url}" -d '#{MultiJson.encode(options, :pretty => Configuration.pretty)}'|
+      logged('update_by_query', curl)
+    end
+  end
+end
