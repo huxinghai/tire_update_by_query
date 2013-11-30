c2f490c5ba945c7f8b49ac62afd0c7ea94ad8488
kaka <huxinghai1988@gmail.com>
2013-11-30 10:20:07 +0800

    fixed refactor

--- lib/tire_update_by_query/index.rb
+++ lib/tire_update_by_query/index.rb
@@ -10,17 +10,25 @@ module Tire
       _params = options.slice(:update)
       options.delete(:update)
       (options[:params] ||= {}).merge!(_params[:update])
-      format = lambda do |opts|
-        f, value, lm = lambda{|k, vl| vl.is_a?(Hash) ? lm.call(k, vl) : value << k }, [], lambda{|k, vl| vl.each{|kl, v| key = (k.empty? ? kl : "#{k}.#{kl}"); f.call(key, v)} }
-        lm.call("", opts)
-        value
-      end
-      options[:script] = format.call(_params[:update]).map{|v| "ctx._source.#{v}=#{v}" }.join(";")
+      options[:script] = format_options(_params[:update]).map{|v| "ctx._source.#{v}=#{v}" }.join(";")
       url = "#{self.url}/#{type}/_update_by_query"
       Configuration.client.post url, MultiJson.encode(options)
     ensure
       curl = %Q|curl -X POST "#{url}" -d '#{MultiJson.encode(options, :pretty => Configuration.pretty)}'|
       logged('update_by_query', curl)
+    end
+
+    private
+    def format_options(opts, key = "", values = [])
+      opts.each do |k, v|
+        _key = key.empty? ? k : "#{key}.#{k}"
+        if v.is_a?(Hash)
+          format_options(v, _key, values)
+        else
+          values << _key
+        end
+      end
+      values
     end
   end
 end
